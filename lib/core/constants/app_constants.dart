c712a323f3a2689fdc92e47170b80f3c33369ec2
DeCode Junkie <97101294+emmanuelochem@users.noreply.github.com>
2023-12-08 08:17:34 +0100

    Update app_constants.dart
    Removed my open api key

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,8 +5,6 @@ class AppConstants {
   static DateFormat formatDate = DateFormat('yMMMMd');
   static DateFormat formatTime = DateFormat.jm();
 
-  //static const String openAIkey =
-  //'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';
   static const String openAIkey =
-      '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';
+      'OPEN API KEY HERE';
 }
